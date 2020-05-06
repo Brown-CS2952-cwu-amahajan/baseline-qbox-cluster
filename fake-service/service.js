709ab3af67d4f29cd3870a93bcad5e84c92d32dc
Akshat Mahajan <akshatm.bkk@gmail.com>
2020-05-06 13:33:16 -0400

    Allow deferred writes

--- fake-service/service.js
+++ fake-service/service.js
@@ -27,6 +27,9 @@ if (process.env.CHILD_QUEUES) {
 var local_log = {}
 var tracked_messages = {}
 
+var active_add_requests = {}
+var active_delete_requests = {}
+
 function parse(str) {
     var args = [].slice.call(arguments, 1),
         i = 0;
@@ -57,7 +60,11 @@ dispatcher.onGet(/^\/saga-add\/[0-9]*/, function(req, res) {
     var id = req.url.split('/').pop()
     id = parseInt(id)
 
-    tracked_messages[id] = 0
+    tracked_messages[id] = {
+        "state": "TRANSACTION",
+        "count": 0,
+        "success": 0
+    }
 
     log_message({"id": id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Received saga-add request"})
     
@@ -79,6 +86,7 @@ dispatcher.onGet(/^\/saga-add\/[0-9]*/, function(req, res) {
                 ch.sendToQueue(children[i], Buffer.from(JSON.stringify({
                     id: id,
                     type: "TRANSACTION",
+                    operation: "ADDITION",
                     parentQueue: process.env.HANDLE_RESPONSES_QUEUE,
                     responseQueue: process.env.HANDLE_RESPONSES_QUEUE,
                 })))
@@ -86,17 +94,19 @@ dispatcher.onGet(/^\/saga-add\/[0-9]*/, function(req, res) {
             }
         })
     })
-
-    log_message({"id": id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Sent back success to caller"})
-
-    res.writeHead(200, {'Content-type': 'application/json'})
-    res.end(JSON.stringify(local_log))
+    active_add_requests[id] = res
 })
 
 dispatcher.onGet(/^\/saga-delete\/[0-9]*/, function(req, res) {
 
     var id = req.url.split('/').pop()
     id = parseInt(id)
+
+    tracked_messages[id] = {
+        "state": "TRANSACTION",
+        "count": 0,
+        "success": 0
+    }
     
     log_message({"id": id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Received saga-delete request"})
 
@@ -106,7 +116,7 @@ dispatcher.onGet(/^\/saga-delete\/[0-9]*/, function(req, res) {
 
     ampq.connect(RABBITMQ_HOST, function(err, conn) {
         conn.createChannel(function(err, ch) {
-            log_message({"id": id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Created publishing channel for compensations"})
+            log_message({"id": id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Created publishing channel for transactions"})
             if (err !== null) {
                 console.error(err);
                 process.exit(1);
@@ -115,18 +125,17 @@ dispatcher.onGet(/^\/saga-delete\/[0-9]*/, function(req, res) {
             for (i = 0; i < children.length; i++) {
                 ch.sendToQueue(children[i], Buffer.from(JSON.stringify({
                     id: id,
-                    type: "COMPENSATION",
+                    type: "TRANSACTION",
+                    operation: "DELETION",
+                    parentQueue: process.env.HANDLE_RESPONSES_QUEUE,
                     responseQueue: process.env.HANDLE_RESPONSES_QUEUE,
                 })))
-                log_message({"id": id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Sent compensating transaction to " + children[i]})
+                log_message({"id": id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Sent transactions to " + children[i]})
             }
         })
     })
 
-    log_message({"id": id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Sent back success to caller"})
-
-    res.writeHead(200, {'Content-type': 'application/json'})
-    res.end(JSON.stringify(local_log))
+    active_delete_requests[id] = res
 })
 
 dispatcher.onGet(/^\/get\/[0-9]*/, function(req, res) {
@@ -154,70 +163,61 @@ ampq.connect(RABBITMQ_HOST, function(err, conn) {
 
             if (!process.env.INTERMEDIATE) {
 
-                if (content.type === "TRANSACTION") {
-                    if (process.env.SHOULD_FAIL) {
-                        log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Failed to insert transaction", "message": content})
-                        ch.sendToQueue(content.responseQueue, Buffer.from(JSON.stringify({
-                            id: content.id,
-                            success: false,
-                            parentQueue: content.parentQueue,
-                            respondingTo: "TRANSACTION",
-                            source: process.env.INCOMING_QUEUE_NAME,
-                        })))
-                        log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Sent false to response queue" + content.responseQueue, "message": content})
-                    } else {
-                        log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Adding to local log", "message": content})
-                        add_id(content.id)
-                        log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Added to local log", "message": content})
-                        ch.sendToQueue(content.responseQueue, Buffer.from(JSON.stringify({
-                            id: content.id,
-                            success: true,
-                            parentQueue: content.parentQueue,
-                            respondingTo: "TRANSACTION",
-                            source: process.env.INCOMING_QUEUE_NAME
-                        })))
-                        log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Sent true to response queue" + content.responseQueue, "message": content})
-                    }
-                }
+                if (process.env.SHOULD_FAIL) {
 
-                if (content.type === "COMPENSATION") {
-                    log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Deleting from local log", "message": content})
-                    delete_id(content.id)
-                    log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Deleted from local log", "message": content})
+                    log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Failed to insert transaction", "message": content})
+                    ch.sendToQueue(content.responseQueue, Buffer.from(JSON.stringify({
+                        id: content.id,
+                        success: false,
+                        parentQueue: content.parentQueue,
+                        respondingTo: content.type,
+                        operation: content.operation,
+                        source: process.env.INCOMING_QUEUE_NAME,
+                    })))
+                    log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Sent false to response queue" + content.responseQueue, "message": content})
+
+                } else {
+
+                    log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Modifying local log", "message": content})
+                    if ((content.type === "TRANSACTION" && content.operation == "DELETION") || (content.type === "COMPENSATION" && content.operation == "ADDITION")) {
+                        delete_id(content.id)
+                    }
+                    if ((content.type === "TRANSACTION" && content.operation == "ADDITION") || (content.type === "COMPENSATION" && content.operation == "DELETION")) {
+                        add_id(content.id)
+                    }
+                    log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Modified local log", "message": content})
+
                     ch.sendToQueue(content.responseQueue, Buffer.from(JSON.stringify({
                         id: content.id,
                         success: true,
-                        respondingTo: "COMPENSATION",
+                        operation: content.operation,
                         parentQueue: content.parentQueue,
+                        respondingTo: content.type,
                         source: process.env.INCOMING_QUEUE_NAME
                     })))
-                    log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Sent true to response queue for compensations" + content.responseQueue, "message": content})
+
+                    log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Sent true to response queue" + content.responseQueue, "message": content})
+
                 }
 
             } else {
 
-                tracked_messages[content.id] = 0
+                tracked_messages[content.id] = {
+                    "state": "TRANSACTION",
+                    "count": 0,
+                    "success": 0
+                }
 
                 for (i = 0; i < children.length; i++) {
                     log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Beginning to issue nested transaction...", "message": content})
-                    if (content.type === "TRANSACTION") {
-                        ch.sendToQueue(children[i], Buffer.from(JSON.stringify({
-                            type: "TRANSACTION",
-                            id: content.id,
-                            parentQueue: content.parentQueue,
-                            responseQueue: process.env.HANDLE_RESPONSES_QUEUE,
-                        })))
-                        log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Issue nested transaction to " + children[i], "message": content})
-                    }
-                    if (content.type === "COMPENSATION") {
-                        ch.sendToQueue(children[i], Buffer.from(JSON.stringify({
-                            id: content.id,
-                            type: "COMPENSATION",
-                            parentQueue: content.parentQueue,
-                            responseQueue: process.env.HANDLE_RESPONSES_QUEUE,
-                        })))
-                        log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Issue nested compensating transaction to " + children[i], "message": content})
-                    }
+                    ch.sendToQueue(children[i], Buffer.from(JSON.stringify({
+                        type: content.type,
+                        id: content.id,
+                        operation: content.operation,
+                        parentQueue: content.parentQueue,
+                        responseQueue: process.env.HANDLE_RESPONSES_QUEUE,
+                    })))
+                    log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Issue nested transaction to " + children[i], "message": content})
                 }
             }
         }
@@ -227,46 +227,72 @@ ampq.connect(RABBITMQ_HOST, function(err, conn) {
 
     ch.assertQueue(process.env.HANDLE_RESPONSES_QUEUE);
     ch.consume(process.env.HANDLE_RESPONSES_QUEUE, function(msg) {
+
         if (msg !== null) {
+
             var content = JSON.parse(msg.content.toString())
+
             log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Received response to transaction", "message": content})
-            if (content.respondingTo !== "COMPENSATION") {
-                if (content.success) {
-                    tracked_messages[content.id] += 1
-                    if (tracked_messages[content.id] === (children.length)) {
-                        if (process.env.INTERMEDIATE && !content.parentQueue.startsWith(process.env.INCOMING_QUEUE_NAME)) {
-                            ch.sendToQueue(content.parentQueue, Buffer.from(JSON.stringify({
-                                id: content.id,
-                                success: true,
-                                parentQueue: content.parentQueue,
-                                source: process.env.INCOMING_QUEUE_NAME
-                            })))
-                            log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Replied success response to parent " + content.parentQueue, "message": content})
-                        }
+            log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "state": tracked_messages[content.id], "message": content})
+
+            if (tracked_messages[content.id]["state"] !== content.respondingTo) {
+                log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Received " + content.respondingTo + " when expecting " + tracked_messages[content.id]["state"] + ", discarding", "message": content})
+                return
+            }
+
+            tracked_messages[content.id]["count"] += 1
+
+            if (tracked_messages[content.id]["count"] === children.length) {
+                if (!content.parentQueue.startsWith(process.env.INCOMING_QUEUE_NAME)) {
+                    ch.sendToQueue(content.parentQueue, Buffer.from(JSON.stringify({
+                        id: content.id,
+                        success: true,
+                        respondingTo: content.respondingTo,
+                        operation: content.operation,
+                        parentQueue: content.parentQueue,
+                        source: process.env.INCOMING_QUEUE_NAME,
+                    })))
+                    log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Replied success response to parent " + content.parentQueue, "message": content})
+                } else {
+
+                    if (content.operation === "ADDITION") {
+                        var queue = active_add_requests
+                    } else {
+                        var queue = active_delete_requests
                     }
+
+                    var res = queue[content.id]
+                    log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Sent back response to caller"})
+                    res.writeHead(tracked_messages[content.id]["status"] === "TRANSACTION" ? 200 : 500, {'Content-type': 'application/json'})
+                    res.end(JSON.stringify(local_log))
+                    delete queue[content.id]
+
                 }
-                if (!content.success) {
-                    tracked_messages[content.id] = 0
+            }
+
+            if (!content.success) {
+
+                if (content.type === "TRANSACTION") {
+
+                    tracked_messages[content.id]["status"] = "COMPENSATION"
+                    tracked_messages[content.id]["count"] = 0
+
                     for (i = 0; i < children.length; i++) {
                         ch.sendToQueue(children[i], Buffer.from(JSON.stringify({
-                            id: content.id,
                             type: "COMPENSATION",
+                            id: content.id,
+                            operation: content.operation,
+                            parentQueue: content.parentQueue,
                             responseQueue: process.env.HANDLE_RESPONSES_QUEUE,
                         })))
-                        log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Issued cancellation to " + children[i], "message": content})
+                        log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Issued compensation to " + children[i], "message": content})
                     }
-                    log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Issued all cancellations", "message": content})
-                    if (process.env.INTERMEDIATE && !content.parentQueue.startsWith(process.env.INCOMING_QUEUE_NAME) ) {
-                        ch.sendToQueue(content.parentQueue, Buffer.from(JSON.stringify({
-                            id: content.id,
-                            success: false,
-                            respondingTo: "TRANSACTION",
-                            parentQueue: content.parentQueue,
-                            source: process.env.INCOMING_QUEUE_NAME
-                        })))
-                        log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Replied success response to parent " + content.parentQueue, "message": content})
-                    }
+                    log_message({"id": content.id, "process": process.env.INCOMING_QUEUE_NAME, "action": "Issued compensation to all", "message": content})
                 }
+
+            } else {
+
+                tracked_messages[content.id]["success"] += 1
             }
         }
     }, {
